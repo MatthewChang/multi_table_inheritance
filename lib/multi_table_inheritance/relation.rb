c986e0065c4de6d8bd0133fc080be00cee5f926f
Matthew Chang <matthew@callnine.com>
2016-10-11 12:34:39 -0400

    in progress

--- lib/multi_table_inheritance/relation.rb
+++ lib/multi_table_inheritance/relation.rb
@@ -6,8 +6,22 @@ module MultiTableInheritance
       self.map(&:specific)
     end
 
-    def where *args
-      byebug
+
+    # Overrides where to find the proper table for delegates query options
+    #
+    # @return [ActiveRecord_Relation] relation with new query options
+    def where args
+      #byebug
+      puts args
+      # new_args = {}
+      # args.each do |k,v|
+      #   if v.is_a? Hash || !klass.mti_delegates_query?(k)
+      #     new_args[k] = v
+      #   else
+      #     new_args[klass.mti_delegate_query(k)] = v
+      #   end
+      # end
+      # super(new_args)
       super
     end
   end
